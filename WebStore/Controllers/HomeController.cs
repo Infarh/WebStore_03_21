9321292cf7b24374d01297c8062e8ade5627d1b0
Infarh <shmachilin@yandex.ru>
2021-03-12 20:32:59 +0300

    Первый контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("First Controller Action");
+        }
+
+        public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
+    }
+}
