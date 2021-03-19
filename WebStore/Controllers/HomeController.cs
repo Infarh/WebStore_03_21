58669b5b5e99f64599ae1770b397097952966094
Infarh <shmachilin@yandex.ru>
2021-03-19 20:07:30 +0300

    Программные соглашения

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using WebStore.Infrastructure.Conventions;
+
 namespace WebStore.Controllers
 {
+    [ActionDescription("Главный контроллер")]
     public class HomeController : Controller
     {
+        [ActionDescription("Главное действие")]
         public IActionResult Index() => View();
 
         public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
