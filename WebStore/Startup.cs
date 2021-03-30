9d1e92844565f6353369e7133834357dab128677
Infarh <shmachilin@yandex.ru>
2021-03-30 20:57:54 +0300

    Хлам

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -7,7 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using WebStore.DAL.Context;
+using WebStore.Data;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.Services;
 using WebStore.Infrastructure.Services.Interfaces;
@@ -16,51 +19,30 @@ namespace WebStore
 {
     public record Startup(IConfiguration Configuration)
     {
-        //private IConfiguration Configuration { get; }
-
-        //public Startup(IConfiguration Configuration)
-        //{
-        //    this.Configuration = Configuration;
-        //}
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<WebStoreDB>(opt => 
+            services.AddDbContext<WebStoreDB>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("Default"))
-                   .EnableSensitiveDataLogging(true)
-                   //.LogTo(Console.WriteLine)
+                //.EnableSensitiveDataLogging(true)
+                //.LogTo(Console.WriteLine)
                 );
+            services.AddTransient<WebStoreDbInitializer>();
 
             services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
             services.AddTransient<IProductData, InMemoryProductData>();
-
-            //services.AddScoped<StoragePrinter>();
-            //services.AddScoped<IPrinter, StoragePrinter>();
-            //services.AddScoped<IMessagesStorage, ListMessageStorage>();
-            //services.AddScoped<IPrinter, StoragePrinter2>();
-
-            //services.AddMvc();
 
             services
                .AddControllersWithViews(
                     mvc =>
                     {
-                        //mvc.Conventions.Add(new ActionDescriptionAttribute("123"));
                         mvc.Conventions.Add(new ApplicationConvention());
                     })
                .AddRazorRuntimeCompilation();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env/*, IServiceProvider services*/)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WebStoreDbInitializer db)
         {
-            //var printer1 = services.GetRequiredService<IPrinter>();
-            //var printer11 = services.GetRequiredService<StoragePrinter>();
-            //var printer2 = services.GetRequiredService<IPrinter>();
-
-            //using (var scope = services.CreateScope())
-            //{
-            //    var printer3 = scope.ServiceProvider.GetRequiredService<IPrinter>();
-            //}
+            db.Initialize();
 
             if (env.IsDevelopment())
             {
@@ -71,9 +53,6 @@ namespace WebStore
 
             app.UseRouting();
 
-            //app.Map()
-            //app.Use()
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/Greetings", async context =>
@@ -81,66 +60,10 @@ namespace WebStore
                     await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
 
-                //endpoints.MapDefaultControllerRoute();
                 endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
             });
         }
-    }
-
-    public interface IPrinter
-    {
-        void Print(string message);
-    }
-
-    class StoragePrinter : IPrinter, IDisposable
-    {
-        private readonly IMessagesStorage _Storage;
-
-        public StoragePrinter(IMessagesStorage Storage) => _Storage = Storage;
-
-        public void Print(string message) => _Storage.Add(message);
-
-        public void Dispose()
-        {
-        }
-    }
-
-    class StoragePrinter2 : IPrinter, IDisposable
-    {
-        private readonly IMessagesStorage _Storage;
-
-        public StoragePrinter2(IMessagesStorage Storage) => _Storage = Storage;
-
-        public void Print(string message) => _Storage.Add(message);
-
-        public void Dispose()
-        {
-        }
-    }
-
-    interface IMessagesStorage
-    {
-        void Add(string message);
-
-        IEnumerable<(DateTime Time, string Message)> GetAll();
-    }
-
-    class ListMessageStorage : IMessagesStorage
-    {
-        private readonly List<(DateTime Time, string Message)> _Messages = new();
-
-        public ListMessageStorage()
-        {
-            
-        }
-
-        public void Add(string message)
-        {
-            _Messages.Add((DateTime.Now, message));
-        }
-
-        public IEnumerable<(DateTime Time, string Message)> GetAll() => _Messages;
     }
 }
