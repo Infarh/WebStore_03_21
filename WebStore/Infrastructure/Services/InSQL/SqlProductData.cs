fa80d601d11d08564c4820873940a733bf7d9bb5
Infarh <shmachilin@yandex.ru>
2021-04-06 20:42:39 +0300

    Детальная информация по товару

--- WebStore/Infrastructure/Services/InSQL/SqlProductData.cs
+++ WebStore/Infrastructure/Services/InSQL/SqlProductData.cs
@@ -32,5 +32,10 @@ namespace WebStore.Infrastructure.Services.InSQL
 
             return query;
         }
+
+        public Product GetProductById(int id) => _db.Products
+           .Include(p => p.Section)
+           .Include(p => p.Brand)
+           .FirstOrDefault(p => p.Id == id);
     }
 }
